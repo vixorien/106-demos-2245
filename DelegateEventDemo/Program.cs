e77625f34bc17e5eb7e5a3a8e3d7423bfe9071a6
Chris Cascioli <chriscascioli@gmail.com>
2025-01-22 13:51:02 -0500

    Added delegate & event demo

---
+++ DelegateEventDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿// Chris Cascioli
+// 1/22/25
+// Demo of delegates and events
+
+namespace DelegateEventDemo
+{
+	// Delegates are often declared outside of classes
+	// so they are available through the entire namespace
+	// - This is essentially a data type we can use later
+	public delegate void LowHealthDelegate(string message, int health);
+
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// Create a tank and hook up it's event
+			// to our fancy function below
+			Tank t = new Tank();
+			t.OnLowHealth += OhNoSomeoneIsLowOnHealth;
+
+			t.TakeDamage(5);
+			t.TakeDamage(20);
+			t.TakeDamage(3);
+			t.TakeDamage(40);
+			t.TakeDamage(15);
+		}
+
+		static void OhNoSomeoneIsLowOnHealth(string message, int health)
+		{
+            Console.WriteLine("Incoming message: " + message);
+            Console.WriteLine("Remaining health: " + health);
+		}
+	}
+}
---
+++ DelegateEventDemo/Tank.cs
@@ -0,0 +1,42 @@
+﻿// Chris Cascioli
+// 1/22/25
+// Example of a class with an event
+
+namespace DelegateEventDemo
+{
+	internal class Tank
+	{
+		// We need an event that can hold (usually) external methods
+		public event LowHealthDelegate OnLowHealth;
+
+		private int health;
+
+		/// <summary>
+		/// Makes a tank w/ 100 health
+		/// </summary>
+		public Tank()
+		{
+			health = 100;
+		}
+
+		/// <summary>
+		/// Reduces tank's health and potentially 
+		/// trigger a "low health" event
+		/// </summary>
+		/// <param name="amount">Incoming damage</param>
+		public void TakeDamage(int amount)
+		{
+            Console.WriteLine("Ouch!");
+
+			health -= amount;
+
+			// We must explicitly check for a "null event",
+			// or we get a crash when there are no subscribers
+			if (health < 20 && OnLowHealth != null)
+			{
+				// Notify the rest of the program
+				OnLowHealth("Dyin' over here, need heals!", health);
+			}
+		}
+	}
+}
