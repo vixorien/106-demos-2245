363b985983bec85fff0dbeeec378f149bea9b31d
Chris Cascioli <chriscascioli@gmail.com>
2025-02-12 16:17:50 -0500

    Added FSM and animation demos

---
+++ FSMDemo/Game1.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+// Chris Cascioli
+// 2/12/25
+// Example of a simple Finite State Machine
+
+namespace FSMDemo
+{
+	enum GameState
+	{
+		Menu,
+		Options,
+		Gameplay,
+		Pause
+	}
+
+	public class Game1 : Game
+	{
+		private GraphicsDeviceManager _graphics;
+		private SpriteBatch _spriteBatch;
+		private SpriteFont font;
+
+		// Tracking which state our program is in
+		private GameState currentState;
+
+		public Game1()
+		{
+			_graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			IsMouseVisible = true;
+		}
+
+		protected override void Initialize()
+		{
+			// Start on the menu
+			currentState = GameState.Menu;
+
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			_spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			font = Content.Load<SpriteFont>("Verdana48");
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			// ALL states need input, so grab it here first
+			KeyboardState kb = Keyboard.GetState();
+			
+			switch (currentState)
+			{
+				case GameState.Menu:
+
+					// Check for transition to the options menu and game play
+					if (kb.IsKeyDown(Keys.O)) { currentState = GameState.Options; }
+					if (kb.IsKeyDown(Keys.Enter)) { currentState = GameState.Gameplay; }
+
+					break;
+
+				case GameState.Options:
+					// Check for transitions that matter in this state
+					if (kb.IsKeyDown(Keys.Escape)) { currentState = GameState.Menu; }
+
+					break;
+
+				case GameState.Gameplay:
+					// Check for transitions that matter in this state
+					if (kb.IsKeyDown(Keys.P)) { currentState = GameState.Pause; }
+
+					break;
+
+				case GameState.Pause:
+					// Check for transition back to gameplay or main menu
+					if (kb.IsKeyDown(Keys.Escape)) { currentState = GameState.Gameplay; }
+					if (kb.IsKeyDown(Keys.X)) { currentState = GameState.Menu; }
+
+					break;
+			}
+
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			// We can begin the sprite batch before
+			// the switch because ALL states need to draw
+			_spriteBatch.Begin();
+
+			switch (currentState)
+			{
+				case GameState.Menu:
+					// "Draw" the main menu
+					_spriteBatch.DrawString(
+						font,
+						"MENU!\n\nO: Options\nEnter: Play game",
+						Vector2.Zero,
+						Color.White);
+					break;
+
+				case GameState.Options:
+					// "Draw" the options screen
+					_spriteBatch.DrawString(
+						font,
+						"OPTIONS!\n\nEsc: Exit",
+						Vector2.Zero,
+						Color.White);
+					break;
+
+				case GameState.Gameplay:
+					// "Draw" the game play
+					_spriteBatch.DrawString(
+						font,
+						"The Game!\n\nP: Pause",
+						Vector2.Zero,
+						Color.White);
+					break;
+
+				case GameState.Pause:
+					// "Draw" the pause screen
+					_spriteBatch.DrawString(
+						font,
+						"PAUSED!\n\nEsc: Resume\nX: Exit",
+						Vector2.Zero,
+						Color.White);
+					break;
+			}
+
+			_spriteBatch.End();
+
+			base.Draw(gameTime);
+		}
+	}
+}
---
+++ SpriteSheetAnimationDemo/Game1.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+// Chris Cascioli
+// 2/12/25
+// Example of frame-by-frame animation with a sprite sheet
+
+namespace SpriteSheetAnimationDemo
+{
+	public class Game1 : Game
+	{
+		private GraphicsDeviceManager _graphics;
+		private SpriteBatch _spriteBatch;
+
+		// Animation variables
+		private const int TotalSpriteSheetFrames = 4;
+		private Texture2D marioSpriteSheet;
+		private int currentFrame;
+		private float animationSpeedFPS;
+		private float secondsPerFrame;
+		private float timeCounter;
+
+		public Game1()
+		{
+			_graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			IsMouseVisible = true;
+		}
+
+		protected override void Initialize()
+		{
+			currentFrame = 1;
+			animationSpeedFPS = 10;
+			secondsPerFrame = 1.0f / animationSpeedFPS;
+			timeCounter = 0;
+
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			_spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			marioSpriteSheet = Content.Load<Texture2D>("MarioSpriteSheet");
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+
+			// How much time has passed?  Keep track
+			timeCounter += (float)gameTime.ElapsedGameTime.TotalSeconds;
+			
+			// If we've surpassed the amount of time for a single frame,
+			// increment our frame counter
+			if (timeCounter >= secondsPerFrame)
+			{
+				currentFrame++;
+				timeCounter -= secondsPerFrame;
+
+				// Loop back to the first frame
+				if (currentFrame >= 4)
+					currentFrame = 1;
+			}
+
+			
+
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.Black);
+
+			// Draw a single "frame" of my sprite sheet
+			_spriteBatch.Begin();
+
+			// Calculate a single source rectangle
+			int frameWidth = marioSpriteSheet.Width / TotalSpriteSheetFrames;
+			Rectangle srcRect = new Rectangle(
+				currentFrame * frameWidth,
+				0,
+				frameWidth,
+				marioSpriteSheet.Height);
+
+
+			Rectangle destRect = new Rectangle(
+				100,
+				100,
+				srcRect.Width * 3,
+				srcRect.Height * 3);
+
+			_spriteBatch.Draw(
+				marioSpriteSheet,
+				destRect,
+				srcRect,
+				Color.White);
+
+			_spriteBatch.End();
+
+			base.Draw(gameTime);
+		}
+	}
+}
