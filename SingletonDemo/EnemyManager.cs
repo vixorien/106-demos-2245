4adc0907ee9cba5c77d2636d0d72383a35a85f52
Chris Cascioli <chriscascioli@gmail.com>
2025-02-19 15:55:25 -0500

    Singleton demo added

---
+++ SingletonDemo/EnemyManager.cs
@@ -0,0 +1,46 @@
+﻿// Actual singleton object implementation
+
+namespace SingletonDemo
+{
+	internal class EnemyManager
+	{
+		/// <summary>
+		/// One any only one instance of the class
+		/// </summary>
+		private static EnemyManager _instance = null!;
+
+		/// <summary>
+		/// Gets the one and only instance of the class
+		/// </summary>
+		public static EnemyManager Instance
+		{
+			get
+			{
+				// If we don't have one yet, make one!
+				if (_instance == null)
+					_instance = new EnemyManager();
+
+				// Return the one and only
+				return _instance;
+			}
+		}
+
+
+		/// <summary>
+		/// Private constructor ensure we can't just
+		/// make these *anywhere* in our code
+		/// </summary>
+		private EnemyManager() { }
+
+
+		public void SpawnEnemy()
+		{
+			Console.WriteLine("Enemy spawned!");
+		}
+
+		public void DestroyEnemy()
+		{
+			Console.WriteLine("Enemy dead!");
+		}
+	}
+}
---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿// Chris Cascioli
+// 2/19/25
+// Example of the singleton design pattern
+
+namespace SingletonDemo
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			EnemyManager.Instance.SpawnEnemy();
+			EnemyManager.Instance.SpawnEnemy();
+			EnemyManager.Instance.SpawnEnemy();
+
+			EnemyManager.Instance.DestroyEnemy();
+
+		}
+	}
+}
